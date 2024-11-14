2f90d53bfc7319cedad687114a8aa8e2da395f29
timothysaatum <timothysaatum@outlook.com>
2024-11-14 11:59:53 +0000

    Remove model file with Git LFS

--- app/main.py
+++ app/main.py
@@ -1,27 +1,34 @@
 from fastapi import FastAPI
 from api.endpoints import report
 import torch
-from torchvision import transforms
+import torchvision.transforms as T
+import torchvision
 
 
-model_path = r"services\best2a_pap_combined_model.pth"
+model_path = r"https://drive.google.com/file/d/1BXpnQ_njm-xFtIBHtd3YgSBQUEfCn50W/view"  # r"services\best2a_pap_combined_model.pth"
 
 app = FastAPI()
-
-
-model = torch.load(model_path, map_location=torch.device("cpu"), weights_only=True)
+device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+class_names = [
+    "background",
+    "superficial",
+    "intermediate",
+    "parabasal",
+    "koilocyte",
+    "endocervical",
+    "endometrial",
+    "squamous metaplastic",
+    "actinomyces",
+]
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(
+    pretrained=False, num_classes=len(class_names), weights=None
+)
+model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+model.to(device)
 model.eval()
 
 # Define image transformations
-transform = transforms.Compose(
-    [
-        transforms.Resize(
-            (224, 224)
-        ),  # Adjust size according to model's input requirements
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
+transform = T.Compose([T.ToTensor()])
 
 # Include the report router
 app.include_router(report.router, prefix="/generate-report")
